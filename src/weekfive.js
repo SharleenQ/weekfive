0b26c0e6b998f6b779d6e09e90b954d96215e1b5
sharleen <sharleen-quinn@live.nmit,ac.nz>
2021-03-22 13:58:47 +1300

    wooooooow today was mindblowing-literally

---
+++ src/weekfive.js
@@ -0,0 +1,23 @@
+const foo = -3 ;
+switch (foo) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+console.log('i like this number')
+break;
+case -1:
+case -2:
+        console.log('i love this number')
+break;
+default:
+    console.log('i don\'t like this number.');
+}
+let isStudent = true
+
+let price = isStudent ? 8 : 12
+console.log(price)
+
+
+
+
